45a051af1c4f44f62525f211af0f778571c4de70
niykko <niyko010@gmail.com>
2025-11-24 12:35:37 +0100

    add test file

---
+++ test_files/denis files/late.sql
@@ -0,0 +1,26 @@
+DROP FUNCTION order_kept_waiting(int,int);
+CREATE FUNCTION order_kept_waiting(suppkey int, orderkey int) RETURNS boolean AS
+$$
+  DECLARE
+    lis   lineitem[];
+    li    lineitem;
+    blame boolean := false; -- is suppkey to blame?
+    multi boolean := false; -- does this order have multiple suppliers?
+  BEGIN
+    lis := $(SELECT array_agg(l)
+            FROM   lineitem AS l
+            WHERE  l.l_orderkey = orderkey)$;
+    FOREACH li IN ARRAY lis LOOP
+      $multi := multi OR li.l_suppkey <> suppkey$;
+      IF $li.l_receiptdate > li.l_commitdate$ THEN
+          IF $li.l_suppkey <> suppkey$ THEN
+            RETURN false;
+          ELSE
+            $blame := true$;
+          END IF;
+      END IF;
+    END LOOP;
+    RETURN $multi AND blame$;
+  END;
+$$
+LANGUAGE PLPGSQL;
